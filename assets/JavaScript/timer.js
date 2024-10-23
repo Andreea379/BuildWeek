1fcb87cd5098a40dff01231b8619f98fbb45cf12
Andreea379 <andreeasamoila379@gmail.com>
2024-10-24 00:07:17 +0200

    refresh page/countdown

--- assets/JavaScript/timer.js
+++ assets/JavaScript/timer.js
@@ -10,4 +10,29 @@ let countDownActive = setInterval(function() {
         }
         countDownValue = countDownValue - 1;
     }, 1000);
-   
+
+
+/****************************il seguente codice ricarica la pagina anche se l'utente non ha cliccato nessun bottone,
+                                     ma il tempo che aveva a disposizine è scaduto.*********************************/
+    
+    /* function refresh() {
+         
+             window.location.reload(true);
+        
+             setTimeout(refresh, 60000);
+     }
+
+     setTimeout(refresh, 60000);
+
+
+
+
+     /* *************************il seguente codice ricarica la pagina quando l'utente a cliccato il bottone************/
+    
+     /*const refresh = document.getElementById("refresh");
+
+    function handleClick() {
+    window.location.reload();
+    }
+    refresh.addEventListener("click", handleClick);
+*/
